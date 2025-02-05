5c3d03e51e8db9cb95dc43a8cbe17ee6ee5eb7eb
GabrielLefort49 <gabriel.phone.2@gmail.com>
2025-02-05 10:27:29 +0100

    feat: comment page and dashboard page link integration

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,14 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\UserRepository;
+use App\Repository\CommentRepository;
+use App\Repository\MovieRepository;
 
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(UserRepository $usersRepository): Response
     {
+        $users = $usersRepository->findAll();
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'users' => $users,
+        ]);
+    }
+    #[Route('/dashboard/comment', name: 'show_comment')]
+    public function show_comment(CommentRepository $commentsRepository): Response
+    {
+        $comments = $commentsRepository->findAll();
+        return $this->render('dashboard/show_comment.html.twig', [
+            'comments' => $comments,
         ]);
     }
 }
