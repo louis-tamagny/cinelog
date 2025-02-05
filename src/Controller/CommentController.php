01fe2a5cd24f4420e16dcec0511c98c2c14f9aa1
Louis Tamagny <louis.tamagny@gmail.com>
2025-02-05 16:38:54 +0100

    feat: add comment new form

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
-use App\Repository\CommentRepository;
 use App\Entity\Comment;
+use App\Form\CommentType;
 use Doctrine\ORM\EntityManagerInterface;
 
 final class CommentController extends AbstractController
@@ -30,5 +33,38 @@ final class CommentController extends AbstractController
         return new Response('', Response::HTTP_NO_CONTENT);
       }
       return $this->redirectToRoute('');
+    }
+
+    #[Route('/movie/{id}/new_comment', name:'movie_new_comment', methods: ['GET', 'POST'])]
+    public function new(Request $request, Movie $movie, EntityManagerInterface $entityManager): Response
+    {
+        // Créer une nouvelle instance de l'entité User
+        $comment = new Comment();
+
+
+        $user = $entityManager->getRepository(User::class)->find(1);
+
+        // Créer le formulaire et le lier à l'entité
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $comment->setCommentUser($user);
+        $comment->setMovie($movie);
+        // Traiter la requête HTTP
+        $form->handleRequest($request);
+
+        // Vérifier si le formulaire est soumis et valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Sauvegarder l'utilisateur dans la base de données
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            // Rediriger vers une autre page ou afficher un message de succès
+            return $this->redirectToRoute('app_comment');
+        }
+
+        // Rendre le formulaire pour qu'il soit affiché à l'utilisateur
+        return $this->render('comment/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
