7eac764fcebea685c6728aec8bfe9e1737699239
ThibaultDiguet <84134140+ThibaultDiguet@users.noreply.github.com>
2025-03-03 17:06:03 +0100

    feat : add response to movie note

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -3,15 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\Movie;
-use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 use App\Entity\Comment;
+use App\Entity\Response as CommentResponse;
 use App\Form\CommentType;
+use App\Repository\MovieRepository;
+use App\Repository\ResponseRepository;
+use App\Repository\CommentRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Service\TmdbService;
 
 final class CommentController extends AbstractController
 {
@@ -74,5 +78,52 @@ final class CommentController extends AbstractController
 
         // Rendre le formulaire pour qu'il soit affiché à l'utilisateur
         return $this->redirectToRoute('movie_details', ['id' => $movie->getTmdbId()], status: Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    #[Route('/comment/{id}/response', name: 'commentResponse')]
+    public function commentResponse(int $id, TmdbService $tmdb, CommentRepository $commentRepository, ResponseRepository $responseRepository, MovieRepository $movieRepository): Response
+    {
+        $comment = $commentRepository->find(['id' => $id]);
+        $tmdbId = $comment->getMovie()->getTmdbId();
+        $movie = $tmdb->getMovieDetails($tmdbId);
+        $responses = $comment->getResponses();
+        return $this->render('comment/response.html.twig', [
+            'comment' => $comment,
+            'movie' => $movie,
+            'responses' => $responses
+        ]);
+    }
+
+    #[Route('/comment/{id}/add_response', name: 'add_response', methods: ['POST'])]
+    public function addResponse(int $id, Request $request, CommentRepository $commentRepository, EntityManagerInterface $entityManager, ResponseRepository $responseRepository): Response
+    {
+        $comment = $commentRepository->find($id);
+        $user = $this->getUser();
+
+        // Empêcher la personne qui a mis l'avis de répondre à son propre commentaire
+        if ($comment->getCommentUser() === $user) {
+          return $this->redirectToRoute('commentResponse', ['id' => $id]);
+        }
+
+        // Empêcher une personne qui a déjà répondu de répondre à nouveau
+        $existingResponse = $responseRepository->findOneBy(['comment' => $comment, 'userResponse' => $user]);
+        if ($existingResponse) {
+            return $this->redirectToRoute('commentResponse', ['id' => $id]);
+        }
+
+        $responseMessage = $request->request->get('response_message');
+
+        if ($responseMessage) {
+            $response = new CommentResponse();
+            $response->setComment($comment);
+            $response->setMessage($responseMessage);
+            $response->setDate(new \DateTime());
+            $response->setUserResponse($this->getUser());
+
+            $entityManager->persist($response);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('commentResponse', ['id' => $id]);
     }
 }
