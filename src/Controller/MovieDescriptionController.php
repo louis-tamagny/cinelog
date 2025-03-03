e424d1ad3dea88e233afc3dbf07c989e519675d7
GabrielLefort49 <gabriel.phone.2@gmail.com>
2025-03-03 15:51:08 +0100

    feat: style page movie detail part 3

--- src/Controller/MovieDescriptionController.php
+++ src/Controller/MovieDescriptionController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Comment;
 use App\Form\CommentType;
+use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -18,17 +19,24 @@ final class MovieDescriptionController extends AbstractController
     }
 
     #[Route('/movie/details/{id}', name: 'movie_details')]
-    public function details(int $id, TmdbService $tmdbService): Response
+    public function details(int $id, TmdbService $tmdbService, CommentRepository $commentRepository): Response
     {
         $movie = $tmdbService->getMovieDetails($id);
         $trailerKey = $tmdbService->getMovieTrailer($id);
-        $comment = new Comment();
-        $form = $this->createForm(CommentType::class, $comment);
+        $createcomment = new Comment();
+        $comments = $commentRepository->findBy(['movie' => $movie]);
+        $movies = [];
+        foreach ($comments as $comment) {
+            $tmdbId = $comment->getMovie();
+            $movies[$tmdbId] = $tmdbService->getMovieDetails($tmdbId);
+        }
+        $form = $this->createForm(CommentType::class, $createcomment);
 
         return $this->render('movie_description/index.html.twig', [
             'movie' => $movie,
             'form' => $form->createView(),
             'trailerKey' => $trailerKey,
+            'comments' => $comments
         ]);
     }
 
