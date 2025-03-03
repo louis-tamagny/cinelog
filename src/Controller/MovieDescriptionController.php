4e9c27c271047d71c5e5b891045b314b665986f3
GabrielLefort49 <gabriel.phone.2@gmail.com>
2025-03-03 17:32:14 +0100

    feat: style for movie detail page part 4

--- src/Controller/MovieDescriptionController.php
+++ src/Controller/MovieDescriptionController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Comment;
 use App\Form\CommentType;
 use App\Repository\CommentRepository;
+use App\Repository\MovieRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -19,24 +21,21 @@ final class MovieDescriptionController extends AbstractController
     }
 
     #[Route('/movie/details/{id}', name: 'movie_details')]
-    public function details(int $id, TmdbService $tmdbService, CommentRepository $commentRepository): Response
+    public function details(int $id, TmdbService $tmdbService, CommentRepository $commentRepository, MovieRepository $movieRepository,UserRepository $userRepository): Response
     {
         $movie = $tmdbService->getMovieDetails($id);
         $trailerKey = $tmdbService->getMovieTrailer($id);
         $createcomment = new Comment();
-        $comments = $commentRepository->findBy(['movie' => $movie]);
-        $movies = [];
-        foreach ($comments as $comment) {
-            $tmdbId = $comment->getMovie();
-            $movies[$tmdbId] = $tmdbService->getMovieDetails($tmdbId);
-        }
+        $moviecomment = $movieRepository->findOneBy(['tmdbId' => $id]);
+        $comments = $commentRepository->findBy(['movie' => $moviecomment]);
+
         $form = $this->createForm(CommentType::class, $createcomment);
 
         return $this->render('movie_description/index.html.twig', [
             'movie' => $movie,
             'form' => $form->createView(),
             'trailerKey' => $trailerKey,
-            'comments' => $comments
+            'comments' => $comments,
         ]);
     }
 
